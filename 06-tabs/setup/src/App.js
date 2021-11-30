43f253742ed1bd41fc2b9dac7013d65ef75d553e
Kwstas Kaskantiris <kkaskantiris@threenitas.com>
2021-12-01 01:48:56 +0200

    completed projects

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,7 +4,63 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading,setLoading] = useState(true);
+  const [jobs,setJobs] = useState([]);
+  const [value,setVlaue] = useState(0);
+
+  const fetchJobs = async () =>{
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() =>{
+    fetchJobs();
+  },[]);
+
+  if(loading){
+    return (
+      <section className="section loading">
+        <h2>loading...</h2>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* {button container} */}
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button key={job.id} onClick={() => setVlaue(index)} className={`job-btn ${index === value && 'active-btn'}`}>{job.company}</button>
+            );
+          })}
+        </div>
+        {/* {job-info} */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty,index) =>{
+            return (
+              <div className="job-desc" key={index}>
+                <FaAngleDoubleRight className="job-info"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App
--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubmenu} = useGlobalContext();
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure dolorem nulla perferendis minima assumenda expedita magni ipsam earum temporibus?</p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="" />
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Hero
